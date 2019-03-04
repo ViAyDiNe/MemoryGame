e73796d47775f88cca5108aa1874a7fb4c189630
ViAyDiNe <neel.augusthy@gmail.com>
2019-03-04 21:31:55 +0800

    created game logic

--- Memory_Game/js/main.js
+++ Memory_Game/js/main.js
@@ -1,18 +1,17 @@
-console.log("Up and running!");
-console.log("User flipped" + " " + cardOne);
-
-cards = ["queen", "queen", "king", "king"];
-cardsInPlay = []
-
-var cardOne = cards[0];
-cardsInPlay.push(cardOne);
-console.log("User flipped queen");
-var cardTwo = cards[2];
-cardsInPlay.push(cardTwo);
-console.log("User flipped king");
-
-if (cardsInPlay.length === 2 && cardsInPlay[0] === cardsInPlay[1]) {
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = [];
+var checkForMatch = function() {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
 	alert("You found a match!");
-} else {
-	alert("Sorry try again.");
-} 
+	} else {
+	alert("Sorry, try again.");
+	}
+};
+var flipCard = function(cardId) {
+	console.log("User flipped " + cards[cardId]);
+	checkForMatch();
+};
+cardsInPlay.push(flipCard);
+
+flipCard(0);
+flipCard(2);
