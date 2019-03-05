da547e11c69f46ec0168c8b509c0d34b0945ff7b
ViAyDiNe <neel.augusthy@gmail.com>
2019-03-06 01:09:16 +0800

    created game logic

--- Memory_Game/js/main.js
+++ Memory_Game/js/main.js
@@ -1,5 +1,27 @@
-var cards = ["queen", "queen", "king", "king"];
+var cards = [
+{
+	rank: "queen",
+	suit: "hearts",
+	cardImage: "images/queen-of-hearts.png"
+},
+{
+	rank: "queen",
+	suit: "diamonds",
+	cardImage: "images/queen-of-diamonds.png"
+},
+{
+	rank: "king",
+	suit: "hearts",
+	cardImage: "images/king-of-hearts.png"
+},
+{
+	rank: "king",
+	suit: "diamonds",
+	cardImage: "images/king-of-diamonds.png"
+}
+];
 var cardsInPlay = [];
+
 var checkForMatch = function() {
 	if (cardsInPlay[0] === cardsInPlay[1]) {
 	alert("You found a match!");
@@ -7,11 +29,18 @@ var checkForMatch = function() {
 	alert("Sorry, try again.");
 	}
 };
+
 var flipCard = function(cardId) {
-	console.log("User flipped " + cards[cardId]);
+	console.log("User flipped " + cards[cardId].rank);
+	console.log(cards[cardId].cardImage);
+	console.log(cards[cardId].suit);
 	checkForMatch();
 };
+
 cardsInPlay.push(flipCard);
 
 flipCard(0);
 flipCard(2);
+
+
+
